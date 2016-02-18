be38f15dabe766d5ce5e5995d57196058314246d
Andrew Tennikoff <atenni@email.com>
2016-02-19 08:59:53 +1100

    Keep date parsing DRY

--- RTO_yearly_counts/CountRTOs.py
+++ RTO_yearly_counts/CountRTOs.py
@@ -12,6 +12,23 @@ START_DATE = datetime.date(year=1970, month=1, day=1)
 csv_date_format = "%d-%b-%y"
 
 
+def parse_date(date_string, is_start_date):
+    try:
+        date = datetime.datetime.strptime(
+                date_string,
+                csv_date_format).date()
+    except ValueError as e:
+        # Date is missing in the CSV. Set to a large/small date to preserve
+        # desired behaviour in `if start_date <= date < end_date` below.
+        if is_start_date:
+            year = 9999
+        else:
+            year = 1900
+        
+        date = datetime.date(year, 1, 1)
+    return date
+
+
 def main(csv_file):
     date_range = [START_DATE.replace(year=year)
                   for year
@@ -21,21 +38,12 @@ def main(csv_file):
     reader = csv.DictReader(csv_file)
     for row in reader:
         # Parse date strings
-        try:
-            start_date = datetime.datetime.strptime(row['Start Date'], csv_date_format)
-            start_date = start_date.date()
-        except ValueError as e:
-            # Start date is missing in the CSV. Set to a large date to preserve
-            # desired behaviour in `if start_date <= date < end_date` below.
-            start_date = datetime.date(9999, 1, 1)
-
-        try:
-            end_date = datetime.datetime.strptime(row['End Date'], csv_date_format)
-            end_date = end_date.date()
-        except ValueError as e:
-            # End date is missing in the CSV. Set to a low date to preserve
-            # desired behaviour in `if start_date <= date < end_date` below.
-            end_date = datetime.date(1900, 1, 1)
+        start_date = parse_date(
+                row['Start Date'],
+                is_start_date=True)
+        end_date = parse_date(
+                row['End Date'],
+                is_start_date=False)
 
         for date in date_range:
             if start_date <= date < end_date:
