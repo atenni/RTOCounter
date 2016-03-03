b9c5a098552b558377b59ca66765938b7c517e05
Brendan Hayward <brendanhayward85@gmail.com>
2016-03-03 15:26:16 +1100

    use basic `hist()` on columns instead of whole dataframe object

--- CountRTOs.r
+++ CountRTOs.r
@@ -22,18 +22,33 @@ endplot = hist(RtoList$End.Date, "years", freq=TRUE)
 plot(startplot, col=rgb(0,0,1,1/4))
 plot(endplot, col=rgb(1,0,0,1/4), add=TRUE)
 
+# THIS IS MUCH CLOSER TO WHAT WE WANT!!!
+hist(RtoList$Start.Date[order(RtoList$Start.Date)],"years",col=rgb(1,1,0,0.2), main="", freq=TRUE)
+par(new=TRUE)
+hist(RtoList$End.Date[order(RtoList$End.Date)],"years",col=rgb(0,1,1,0.2), main="", xlab="", ylab="", axes=FALSE, freq=TRUE)
+
 # Get number of current RTOs
 current_RTOs <- function(rto_frame)
 {
-        # do the count here
-        '
-        histogram of RTOs where status is current
-        apply a mask or filter to:
-        hist(RtoList$Start.Date)
-        subset(rto_frame, rto_frame$Status == "Current")
-        OR
-        qplot(rto_frame.Start$Date - rto_frame.End$Date, geom="histogram")
-        '
-        qplot(rto_frame$Start.Date, geom="histogram", binwidth=250,
-            fill=I("blue"), alpha=I(0.25))
+    # do the count here
+    '
+    histogram of RTOs where status is current
+    i.e. Starting - (Starting intersect Ending)
+    apply a mask or filter to:
+    hist(RtoList$Start.Date)
+    subset(rto_frame, rto_frame$Status == "Current")
+    OR
+    qplot(rto_frame.Start$Date - rto_frame.End$Date, geom="histogram")
+    '
+    # use pretty ggplot2
+    qplot(rto_frame$Start.Date, geom="histogram", binwidth=250,
+        fill=I("blue"), alpha=I(0.25))
+
+    # seems to do what I want, but will get lengthy when formatted
+    hist(rto_frame$Start.Date,"quarters", freq=TRUE) - hist(rto_frame$End.Date, "quarters", freq=TRUE)
+
+    # overlapping method
+    hist(rto_frame$Start.Date,"quarters", freq=TRUE, col=rgb(1,1,0,0.7), main="", xlab="started")
+    par(new=TRUE)
+    hist(rto_frame$End.Date, "quarters", freq=TRUE, col=rgb(0,1,1,0.4), main="", xlab="ended")
 }
